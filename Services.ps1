8c3385bcf8116fa53cdebb2064349791df91a1a4
praiselily <222109493+praiselily@users.noreply.github.com>
2025-10-11 17:56:11 +0200

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -56,7 +56,7 @@ foreach ($svc in $services) {
             Write-Host ("  {0,-12} {1,-40}" -f $svc.Name, $displayName) -ForegroundColor Green -NoNewline
             
             if ($svc.Name -eq "Bam") {
-                Write-Host " | Enabled" -ForegroundColor Yellow
+                Write-Host " | Enabled" -ForegroundColor Gray
             } else {
                 try {
                     $process = Get-CimInstance Win32_Service -Filter "Name='$($svc.Name)'" | Select-Object ProcessId
